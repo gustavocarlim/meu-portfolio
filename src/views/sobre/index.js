70d7518317857b6d410676e90aa2aea596dd2d0f
gustavocarlim <gustavocarlimsaens@gmail.com>
2024-02-27 22:33:28 -0300

    corrigindo texto

--- src/views/sobre/index.js
+++ src/views/sobre/index.js
@@ -13,9 +13,12 @@ const SobreMim = () => {
               <span className="title__section-title">FullStack</span>
             </div>
             <div className="about__section">
+              <div className="mobile__descript">
+                <p>Sou um desenvolvedor júnior FullStack com mais de um ano de experiência estudando e prestando serviço na área de tecnologia. Atualmente estou cursando graduação em Análise e Desenvolvimento de Sistemas.</p>
+              </div>
               <div className="desktop__descript">
                 <p>Sou um desenvolvedor júnior FullStack com mais de um ano de experiência estudando e</p>
-                <p>prestando serviço   na área de tecnologia. Completei o programa de formação na Trybe e </p>
+                <p>prestando serviço na área de tecnologia. Completei o programa de formação na Trybe e </p>
                 <p>atualmente estou cursando graduação em Análise e Desenvolvimento de Sistemas.</p>
               </div>
             </div>
